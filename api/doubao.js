c65a7feea67746f39dd2ac6a52b372d85f83be54
wangqiongyao <wowqynono@163.com>
2025-11-27 14:25:41 +0800

    豆包api模型替换

--- api/doubao.js
+++ api/doubao.js
@@ -35,7 +35,7 @@ export default async function handler(request, response) {
         const userInput = request.body;
         const requestBody = {
             ...userInput,
-            model: userInput.model || "doubao-seedream-4-0-250828",
+            model: userInput.model || "doubao-seedream-3-0-t2i-250415",
             stream: false,
             watermark: false
         };
