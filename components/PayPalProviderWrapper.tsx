e9097356ece8431900430eada05bac60ef97bf69
zhangtao <git.vistaflyer.com>
2026-03-11 11:01:34 +0800

    修改

--- components/PayPalProviderWrapper.tsx
+++ components/PayPalProviderWrapper.tsx
@@ -8,7 +8,8 @@ export default function PayPalProviderWrapper({
     children: React.ReactNode;
 }) {
     // 💡 Sanitize Client ID: Remove quotes if they exist in the env var
-    let clientId = "AQgGwtwMZuLeZGpzyslyWLBa_wC5VRJNkXiMs4mgK-91MoWGuBez4-lvtjJacVftQ_qD1ZdxGN4_yCb6";
+    // let clientId = "AQgGwtwMZuLeZGpzyslyWLBa_wC5VRJNkXiMs4mgK-91MoWGuBez4-lvtjJacVftQ_qD1ZdxGN4_yCb6";
+    let clientId  = "AWadfwOBXGEALRr0IyOd0oyYm-D16yyoWkPED0FoH-KPJjVVaZuE8iNq-nwG_vCPpR_DnLGiGU5Cmj9F";
     clientId = clientId.replace(/^"|"$/g, '').trim();
 
     const paypalOptions = {
